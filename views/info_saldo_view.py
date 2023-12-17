9a871abb50d2232ff2f9d14207fb1b2a1937e3ee
abdanhafidz <abdan.hafidz@gmail.com>
2023-12-17 13:20:24 +0700

    feat: add info saldo tableView

--- views/info_saldo_view.py
+++ views/info_saldo_view.py
@@ -4,5 +4,7 @@ import pandas
 def Main():
     print("============ INFORMASI KEUANGAN @ SISTEM INFORMASI PENCATATAN KAS RKA ============")
     print("Catatan Kas :")
-    print(pandas.DataFrame(kas_data.kas))
+    for i in range(0,len(kas_data.kas)):
+        info = kas_data.kas[i]
+        print(pandas.DataFrame(info,index=[0]))
     view.loadView(0)
